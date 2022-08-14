5d276dd32e71700f1a361d86ada600a771f4bfc5
ItIsSunny385 <ema.minoru@gmail.com>
2022-08-14 15:32:18 +0900

    ヘッダーを作成

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header className="text-gray-600 body-font bg-gray-200">
+        <div className="container mx-auto flex flex-wrap p-1 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900">
+            <span className="ml-3 text-xl">地図マネージャー</span>
+          </a>
+        </div>
+      </header>
+      <main className="body-font">
+        <div className="container p-1">
+          <Component {...pageProps} />
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
