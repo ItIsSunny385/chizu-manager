d7267ad97df4ccda13945e4e33cce96806c22c64
ItIsSunny385 <ema.minoru@gmail.com>
2022-08-17 13:22:15 +0900

    システム名をリンクにする

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <header className="text-gray-600 body-font bg-gray-200">
         <div className="container flex flex-wrap p-3 flex-col flex-row">
-          <a className="flex title-font font-medium items-center text-gray-900">
-            <span className="ml-3 text-xl">地図マネージャー</span>
-          </a>
+          <Link href="/">
+            <a className="flex title-font font-medium items-center text-gray-900">
+              <span className="ml-3 text-xl">地図マネージャー</span>
+            </a>
+          </Link>
         </div>
       </header>
       <main className="body-font">
