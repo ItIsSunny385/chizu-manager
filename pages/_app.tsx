c963c6aae09e60b723ad69e8e1381f1e71e503f7
ItIsSunny385 <ema.minoru@gmail.com>
2022-08-19 19:33:48 +0900

    fontawesome を削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGear } from "@fortawesome/free-solid-svg-icons";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -26,8 +24,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           </div>
           <div>
             <Link href="/setting">
-              <a className="inline-block text-xl px-3 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-purple-500 hover:bg-white mt-0">
-                <FontAwesomeIcon icon={faGear} />
+              <a className="inline-block text-base px-3 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-purple-500 hover:bg-white mt-0">
+                設定
               </a>
             </Link>
           </div>
