664b8ea8c97bf458313269c3bb7e86636f3345cb
ItIsSunny385 <ema.minoru@gmail.com>
2022-08-21 08:09:19 +0900

    Modal を最低限準備

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,8 @@
+import { MouseEventHandler } from "react";
+
 interface Props {
   children: string;
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
 const Button = (props: Props) => {
@@ -7,6 +10,7 @@ const Button = (props: Props) => {
     <button
       className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-3 rounded text-right"
       type="button"
+      onClick={props.onClick}
     >
       {props.children}
     </button>
--- pages/setting.tsx
+++ pages/setting.tsx
@@ -1,14 +1,22 @@
 import type { NextPage } from "next";
-
-const statuses = [
-  { name: "未訪問", color: "緑" },
-  { name: "不在", color: "水色" },
-  { name: "会えた", color: "オレンジ" },
-  { name: "外国人", color: "黄色" },
-  { name: "拒否", color: "赤" },
-];
+import { useEffect, useState } from "react";
+import Button from "../components/Button";
+import Modal from "../components/Modal";
+import { Status } from "../types/db";
+import { getAllStatus, getDB } from "../utils/db";
 
 const Setting: NextPage = () => {
+  const [statuses, setStatuses] = useState<Status[]>();
+  const [displayStatusModal, setDisplayStatusModal] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const db = await getDB();
+      const newStatuses = await getAllStatus(db);
+      setStatuses(newStatuses);
+    })();
+  }, []);
+
   return (
     <>
       <div className="my-5">
@@ -17,51 +25,79 @@ const Setting: NextPage = () => {
             ステータス
           </h2>
         </div>
-        <table className="table-auto w-full text-left whitespace-no-wrap">
-          <thead>
-            <tr>
-              <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
-                ステータス名
-              </th>
-              <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
-                色
-              </th>
-              <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {statuses.map((x, i) => (
-              <tr key={x.name}>
-                <td
-                  className={`${
-                    i !== 0 ? "border-t-2 border-gray-200" : ""
-                  } px-4 py-3`}
-                >
-                  {x.name}
-                </td>
-                <td
-                  className={`${
-                    i !== 0 ? "border-t-2 border-gray-200" : ""
-                  } px-4 py-3`}
-                >
-                  {x.color}
-                </td>
-                <td
-                  className={`${
-                    i !== 0 ? "border-t-2 border-gray-200" : ""
-                  } px-4 py-3`}
-                >
-                  <span>
-                    <a href="#">編集</a>
-                  </span>
-                  <span className="ml-2">
-                    <a href="#">削除</a>
-                  </span>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div>
+          {statuses == null ? (
+            <p className="text-center">データ取得中です。</p>
+          ) : statuses.length === 0 ? (
+            <p className="text-center">データがありません。</p>
+          ) : (
+            <table className="table-auto w-full text-left whitespace-no-wrap">
+              <thead>
+                <tr>
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                    ステータス名
+                  </th>
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                    色
+                  </th>
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {statuses.map((x, i) => (
+                  <tr key={x.name}>
+                    <td
+                      className={`${
+                        i !== 0 ? "border-t-2 border-gray-200" : ""
+                      } px-4 py-3`}
+                    >
+                      {x.name}
+                    </td>
+                    <td
+                      className={`${
+                        i !== 0 ? "border-t-2 border-gray-200" : ""
+                      } px-4 py-3`}
+                    >
+                      {x.color}
+                    </td>
+                    <td
+                      className={`${
+                        i !== 0 ? "border-t-2 border-gray-200" : ""
+                      } px-4 py-3`}
+                    >
+                      <span>
+                        <a href="#">編集</a>
+                      </span>
+                      <span className="ml-2">
+                        <a href="#">削除</a>
+                      </span>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+        <div className="ml-auto text-right">
+          <Button onClick={() => setDisplayStatusModal(true)}>新規追加</Button>
+        </div>
+        {displayStatusModal && <Modal />}
+      </div>
+      <hr className="my-5" />
+      <div className="my-5">
+        <div className="my-3">
+          <h2 className="text-2xl font-medium title-font mb-2 text-gray-900">
+            中心座標
+          </h2>
+        </div>
+      </div>
+      <hr className="my-5" />
+      <div className="my-5">
+        <div className="my-3">
+          <h2 className="text-2xl font-medium title-font mb-2 text-gray-900">
+            Google Map
+          </h2>
+        </div>
       </div>
     </>
   );
