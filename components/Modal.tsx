cc968eecc8c7ac898cea652f49f9d4bc1ac95cd9
ItIsSunny385 <ema.minoru@gmail.com>
2022-08-22 21:25:45 +0900

    モーダルを修正

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -8,18 +8,19 @@ interface Props {
 const Modal = (props: Props) => {
   return (
     <div
-      className="relative z-10"
+      className="relative"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
+      style={{ zIndex: 10000 }}
     >
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-      <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="fixed inset-0 overflow-y-auto" style={{ zIndex: 10000 }}>
         <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
           <div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start w-full">
-                <div className="mt-3 text-center sm:mt-0 sm:text-left w-full">
+                <div className="mt-3 sm:mt-0 sm:text-left w-full">
                   <h3
                     className="text-xl leading-6 font-medium text-gray-900"
                     id="modal-title"
