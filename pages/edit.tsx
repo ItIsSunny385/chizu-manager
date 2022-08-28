e0c0898e38e39db3b00b345b5d7fc0d28b72f30a
ItIsSunny385 <ema.minoru@gmail.com>
2022-08-28 09:48:40 +0900

    バグを修正

--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useDB } from "../utils/hook";
 const MapMain = dynamic(() => import("../components/MapMain"), {
   loading: () => <p>A map is loading</p>,
@@ -11,15 +11,9 @@ const MapMain = dynamic(() => import("../components/MapMain"), {
 const Edit: NextPage = () => {
   const db = useDB();
   const router = useRouter();
-  const [id, setId] = useState<string>();
 
-  useEffect(() => {
-    const nextId = router.query.id;
-    setId(nextId != null ? nextId.toString() : undefined);
-  }, []);
-
-  return db != null && id != null ? (
-    <MapMain db={db} id={id} />
+  return db != null && router.query.id != null ? (
+    <MapMain db={db} id={router.query.id.toString()} />
   ) : (
     <React.Fragment />
   );
